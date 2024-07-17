40790cff3793c30436aedfa7eadf79dfd1f596e1
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-07-18 00:17:51 +0530

    feat: implement merge sort

--- Algorithms/Sorting/merge-sort.py
+++ Algorithms/Sorting/merge-sort.py
@@ -1,36 +1,35 @@
-from typing import List
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
 
+        merge_sort(left_half)
+        merge_sort(right_half)
 
-def merge_sorted_arrays(arr1: List[int], arr2: List[int]) -> List[int]:
-    """
-    Merges two sorted arrays into a single sorted array.
+        i = j = k = 0
 
-    :param arr1: First sorted array.
-    :param arr2: Second sorted array.
-    :return: A merged sorted array.
-    """
-    i, j = 0, 0
-    m, n = len(arr1), len(arr2)
-    merged_arr = []
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
 
-    while i < m and j < n:
-        if arr1[i] < arr2[j]:
-            merged_arr.append(arr1[i])
+        while i < len(left_half):
+            arr[k] = left_half[i]
             i += 1
-        else:
-            merged_arr.append(arr2[j])
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
             j += 1
-
-    # Append remaining elements (if any)
-    merged_arr += arr1[i:]
-    merged_arr += arr2[j:]
-
-    return merged_arr
+            k += 1
 
 
 # Example usage
-arr1 = [1, 3, 5, 7, 9, 11]
-arr2 = [2, 4, 6, 8, 10, 12]
-
-sorted_arr = merge_sorted_arrays(arr1, arr2)
-print(sorted_arr)
+arr = [12, 11, 13, 5, 6, 7]
+merge_sort(arr)
+print(arr)  # Output: [5, 6, 7, 11, 12, 13]
